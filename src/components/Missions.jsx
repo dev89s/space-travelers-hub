a4c11e6f2b4212ce15f161d1e68892fb19fa3ef9
Ouail-01 <ouaillaarabi@gmail.com>
2023-05-16 14:16:25 +0100

    Display the missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,7 +1,45 @@
-function Missions() {
+/* eslint-disable */
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { missionLoad } from '../redux/missions/missionsSlice';
+import MissionProps from './MissionsProps';
+import "../style/Mission.css"
+
+const Missions = () => {
+  const missions = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (missions.length === 0) {
+      dispatch(missionLoad());
+    }
+  });
+
   return (
-    <div>Mission page</div>
+    <>
+      <table className="missionTable">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <MissionProps
+              key={mission.id}
+              id={mission.id}
+              missionName={mission.name}
+              description={mission.description}
+              reserved={mission.reserved}
+            />
+          ))}
+        </tbody>
+      </table>
+    </>
   );
-}
+};
 
 export default Missions;
