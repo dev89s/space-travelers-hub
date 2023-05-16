776047f0cfe44e92398df5e0718c90523a6d3933
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-05-16 11:22:01 +0300

    Fetch & initial Display of rockets

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,6 +1,37 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
+
 function Rockets() {
+  const { rockets } = useSelector((state) => state.rockets);
+  const { isLoading } = useSelector((state) => state.rockets);
+  const { error } = useSelector((state) => state.rockets);
+  const { listState } = useSelector((state) => state.rockets);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (listState !== 'loaded') {
+      dispatch(fetchRockets());
+    }
+  }, [dispatch, listState]);
+
+  if (error) {
+    return (
+      <h2>
+        There was an error loading the page.
+        <br />
+        {error}
+      </h2>
+    );
+  }
+
+  if (isLoading) {
+    return (<h2>Page is loading</h2>);
+  }
   return (
-    <div>Rockes page</div>
+    <ul>
+      {rockets.map((rocket) => (<li key={rocket.id}>{rocket.name}</li>))}
+    </ul>
   );
 }
 
