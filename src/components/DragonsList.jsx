0a0871b1d062978dae6494e7e24d282e732e2694
Abdurahim .M <Abdugreat2@gmail.com>
2023-05-17 22:02:47 +0300

    Reserve dragon and cancel reservation functionality added

--- src/components/DragonsList.jsx
+++ src/components/DragonsList.jsx
@@ -1,23 +1,24 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchDragons, dragonSelector } from '../redux/dragons/dragonsSlice';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDragons, dragonSelector } from "../redux/dragons/dragonsSlice";
 
-import style from '../style/DragonList.module.css';
-import Dragon from './Dragon';
+import style from "../style/DragonList.module.css";
+import Dragon from "./Dragon";
 
 function DragonsList() {
   const dispatch = useDispatch();
-  const { dragons } = useSelector(dragonSelector);
-
+  const { dragons, listState } = useSelector(dragonSelector);
   useEffect(() => {
-    dispatch(fetchDragons());
+    listState === "idle" && dispatch(fetchDragons());
   }, [dispatch]);
 
   return (
-    <div className={style['list-container']}>
+    <div className={style["list-container"]}>
       {dragons.map((dragon) => (
         <Dragon
           key={dragon.id}
+          id={dragon.id}
+          reserved={dragon.reserved}
           imgURL={dragon.flickr_images[1]}
           name={dragon.name}
           description={dragon.description}
