46ecc3c07cd9505a5cb2437959371686ee74d81f
Ouail-01 <ouaillaarabi@gmail.com>
2023-05-18 09:31:24 +0100

    Add the missions in profile page

--- src/components/My-Profile.jsx
+++ src/components/My-Profile.jsx
@@ -4,6 +4,10 @@ import { useSelector } from 'react-redux';
 
 function MyProfile() {
   const { rockets } = useSelector((state) => state.rockets);
+
+  const missions = useSelector((state) => state.missions);
+  const reservedMissions = missions.filter((item) => item.reserved === true);
+
   return (
     <div className="profile-page">
       <section className="profile-rocket-section">
@@ -20,6 +24,26 @@ function MyProfile() {
               {rockets.filter((rocket) => rocket.reserved).map((rocket) => (
                 <li key={rocket.id} className="profile-rocket-item">
                   {rocket.name}
+                </li>
+              ))}
+            </ul>
+          )}
+      </section>
+
+      <section className="profile-mission-section">
+        <h2 className="profile-mission-header">My Mission</h2>
+        {(reservedMissions.length === 0)
+          && (
+            <h2 className="no-reserved-mission">
+              You have no reserved mission!
+            </h2>
+          )}
+        {(reservedMissions.length > 0)
+          && (
+            <ul className="reserved-mission-list">
+              {reservedMissions.map((item) => (
+                <li key={item.id} className="profile-mission-item">
+                  {item.name}
                 </li>
               ))}
             </ul>
