88992d47fff86d2c610f538cf5deddcbd6ab7c9b
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-05-18 20:33:46 +0300

    Fix Linter errors

--- src/tests/My-Profile.test.js
+++ src/tests/My-Profile.test.js
@@ -1,19 +1,14 @@
 import { Provider } from 'react-redux';
-import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import MyProfile from '../components/My-Profile';
 import store from '../redux/store';
 
-describe('snapshot test of profile page', () => {
-  it('snapshot test for my profile page', () => {
-    const myProfile = renderer.create(
-      <Provider store={store}>
-        <MyProfile />
-      </Provider>
-    );
-
-    const tree = myProfile.toJSON();
-
-    expect(tree).toMatchSnapshot();
-  });
+it('snapshot test for my profile page', () => {
+  const myProfile = renderer.create(
+    <Provider store={store}>
+      <MyProfile />
+    </Provider>,
+  );
+  const tree = myProfile.toJSON();
+  expect(tree).toMatchSnapshot();
 });
